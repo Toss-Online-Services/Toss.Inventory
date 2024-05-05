5753b2a06c2c7e487273bbea30a72d3bed5932e3
gontsemoses <moses.gontse@tossonline.co.za>
2024-05-05 14:44:46 +0200

    struc

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddKeyVaultIfConfigured(builder.Configuration);
 
-builder.Services.AddApplicationServices();
+builder.Services.AddTodoApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddWebServices();
 
