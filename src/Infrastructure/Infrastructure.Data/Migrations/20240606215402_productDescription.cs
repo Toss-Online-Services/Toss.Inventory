cd1cd101460bd150cc506f05398d4da0d3a8f767
gontsemoses <moses.gontse@tossonline.co.za>
2024-06-06 23:54:22 +0200

    Add migration

---
+++ src/Infrastructure/Infrastructure.Data/Migrations/20240606215402_productDescription.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eShop.Catalog.API.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class productDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gtin",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ManufacturerPartNumber",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sku",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VendorPartNumber",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DisplayStockAvailability",
+                table: "ProductInventories",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DisplayStockQuantity",
+                table: "ProductInventories",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "UseMultipleWarehouses",
+                table: "ProductInventories",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Gtin",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ManufacturerPartNumber",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Sku",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "VendorPartNumber",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "DisplayStockAvailability",
+                table: "ProductInventories");
+
+            migrationBuilder.DropColumn(
+                name: "DisplayStockQuantity",
+                table: "ProductInventories");
+
+            migrationBuilder.DropColumn(
+                name: "UseMultipleWarehouses",
+                table: "ProductInventories");
+        }
+    }
+}
