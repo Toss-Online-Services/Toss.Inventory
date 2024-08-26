481ab9198112c7c703ae4c6f4b4b5e820d0ed684
gontsemoses <moses.gontse@tossonline.co.za>
2024-08-26 14:40:08 +0200

    Latest

---
+++ src/Application/Common/Interfaces/IUser.cs
@@ -0,0 +1,6 @@
+﻿namespace Toss.Inventory.Application.Common.Interfaces;
+
+public interface IUser
+{
+    string? Id { get; }
+}
---
+++ src/Domain/Common/BaseEvent.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace Toss.Inventory.Domain.Common;
+
+public abstract class BaseEvent : INotification
+{
+}
